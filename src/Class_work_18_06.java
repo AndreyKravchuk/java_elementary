899920a54137489a1e30db37ee720ded637e43fc
albaderon <kravchukodessa@gmail.com>
2017-06-19 20:33:40 +0300

    klass_1

---
+++ src/Class_work_18_06.java
@@ -0,0 +1,34 @@
+/**
+ * Created by java6 on 19.06.17.
+ */
+public class Class_work_18_06 {
+    public static void main(String[] args) {
+//        int b = 121;
+//        System.out.println(b>>1);
+//        System.out.println(b>>2);
+//        System.out.println(b>>3);
+//        System.out.println(b>>4);
+
+//        int a = Integer.MAX_VALUE - 2;
+//        int b = Integer.MAX_VALUE - 1;
+//
+//        System.out.println(a);
+//        System.out.println(b);
+//
+//        b += a;
+//        a=b-a;
+//        b-=a;
+//
+//        System.out.println(a);
+//        System.out.println(b);
+
+//          float f = 0.5f;
+//          int i = (int)f;
+//        System.out.println(i);
+
+
+
+
+    }
+
+}
