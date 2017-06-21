fab01469486fde47552919f0ff1b67a10d291380
albaderon <kravchukodessa@gmail.com>
2017-06-21 20:23:27 +0300

    First home work, Task 2

---
+++ src/DoubleStars.java
@@ -0,0 +1,13 @@
+/**
+ * Created by My comp on 21.06.2017.
+ */
+public class DoubleStars {
+    public static void main(String[] args) {
+
+        for (int x = 1; x < 6; x++) {
+            for (int j = 1; j < x; j++)
+                System.out.print("**");
+            System.out.println();
+        }
+    }
+}
