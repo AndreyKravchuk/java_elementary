5151fe75546c37d7d1f35a2c92e8abe4a7ee3740
albaderon <kravchukodessa@gmail.com>
2017-07-30 14:41:40 +0300

    Test - Enum

---
+++ src/EnumColours.java
@@ -0,0 +1,15 @@
+/**
+ * Created by user on 27.07.2017.
+ */
+public enum EnumColours {
+    RED(15),
+    GREEN(25),
+    BLUE(10);
+
+    public final int coast;
+
+    EnumColours(int coast){
+        this.coast = coast;
+    }
+
+}
