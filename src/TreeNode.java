cf3b6559169b40877ec0f2b87f05d582856f5d11
albaderon <kravchukodessa@gmail.com>
2017-08-09 19:36:54 +0300

    Binary TreeNod

---
+++ src/TreeNode.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+/**
+ * Created by user on 09.08.2017.
+ */
+public class TreeNode {
+
+    Integer value;
+    TreeNode up;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int value){
+        this.value = value;
+    }
+
+}
