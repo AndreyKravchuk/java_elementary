875b2a34f477902994a2b813a24aaf66991ffcaf
kravchuk <kravchukodessa@gmail.com>
2017-08-28 13:19:51 +0300

    Test

--- src/ClassWork/Read/Main.java
+++ src/ClassWork/Read/Main.java
@@ -11,15 +11,13 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        ConsoleReader consoleReader = new ConsoleReader();
-        MyFileReader myFileReader = new MyFileReader();
-        String word = "World";
-        File folder = new File("/home/java6/IdeaProjects/java_elementary1/src/ClassWork/Read");
-        FileScanner fileScanner = new FileScanner(folder);
-//        System.out.println(consoleReader.readMyFile());
-//        System.out.println(myFileReader.readMyFile());
-        System.out.println(fileScanner.scan(word));
+//        ConsoleReader read1 = new ConsoleReader();
+//        System.out.println(read1.readFile());
 
+//        MyFileReader read2 = new MyFileReader();
+//        System.out.println(read2.readFile());
+     File file1 = new File("C:/Users/kravchuk/IdeaProjects/java_elementary/src/ClassWork/Read/Directory");
+     MyFileScanner read3 = new MyFileScanner();
+     System.out.println(read3.recursiveScanner(file1,"World"));
     }
-
 }
