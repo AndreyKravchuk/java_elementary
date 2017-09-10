95f6ea9d92a8a04a40bf2cd2d608fab64749c31f
kravchuk <kravchukodessa@gmail.com>
2017-09-10 21:30:58 +0300

    Main configuration

--- src/ClassWork/Test_XML/TestConfiguration.java
+++ src/ClassWork/Test_XML/TestConfiguration.java
@@ -15,22 +15,48 @@ import java.util.List;
 public class TestConfiguration {
     public static void main(String[] args) throws IOException, SAXException, ParserConfigurationException, ParseException {
 
-        MyConfiguration configuration = MyConfiguration.create("/home/java6/IdeaProjects/java_elementary1/src/ClassWork/Test_XML/conf.xml");
+        MyConfiguration configuration = MyConfiguration.create("C:/Users/kravchuk/IdeaProjects/java_elementary/src/ClassWork/Test_XML/conf.xml");
         System.out.println(configuration.getDirectory());
         System.out.println(configuration.getExtension());
         File myFile = new File(configuration.getDirectory());
         File [] path = myFile.listFiles();
-        for (int i=0; i < path.length; i++ ){
-            if (path[i].getName().endsWith(configuration.getExtension())){
-                try (BufferedReader br = new BufferedReader(new FileReader(path[i]));
-                     
 
+        JSONParser jsonParser = new JSONParser();
+        JSONObject jsonObject = (JSONObject) jsonParser.parse(new FileReader("C:/Users/kravchuk/IdeaProjects/java_elementary/src/ClassWork/Test_XML/output.json"));
 
+        String myString = "";
 
+        for (int i = 0; i < path.length; i++) {
+
+            if (path[i].getName().endsWith(configuration.getExtension())) {
+                try (BufferedReader reader = new BufferedReader(new FileReader(path[i]))){
+                    myString += reader.readLine();
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
                 }
             }
-        }
 
+
+        }
+        String upperCase = "";
+        String lowerCase = "";
+        for (int j = 0; j < myString.length(); j++ ){
+
+            if (Character.isUpperCase(myString.charAt(j))){
+                upperCase += myString.charAt(j);
+
+            } else if (Character.isLowerCase(myString.charAt(j))) {
+                lowerCase += myString.charAt(j);
+            }
+        }
+        jsonObject.put("upper",upperCase);
+        jsonObject.put("lower",lowerCase);
+
+        System.out.println(jsonObject);
+
+        try (FileWriter fileWriter = new FileWriter("C:/Users/kravchuk/IdeaProjects/java_elementary/src/ClassWork/Test_XML/output.json")) {
+            jsonObject.writeJSONString(fileWriter);
+        }
 
 
     }
